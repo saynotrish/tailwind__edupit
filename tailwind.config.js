513840d620c24e9d56bed18855b3c88d4ae35c7d
saynotrish <anuran.nyleco24@gmail.com>
2023-06-08 15:40:40 +0800

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark: "hsl(208.7,51.11%,17.65%)",
+        light: "hsl(208.7,51.11%,17.65%)",
+        primary: "#0eb582",
+        textDark: "#2a374c",
+        grey: "hsl(0,0%,96.86%)",
+      },
+      keyframes: {
+        sliding1:{
+          from: {
+            left:'0',
+          },
+          to: {
+            left:'100%',
+          },
+        },
+        sliding2:{
+          from: {
+            right:'0',
+          },
+          to: {
+            right:'100%',
+          },
+        }
+      },
+      animation:{
+        sliding1: 'sliding1 3s linear infinite',
+        sliding2: 'sliding2 3s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
